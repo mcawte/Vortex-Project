563db4d552294cc3180f388093c8145229b7029f
Michael Cawte <mcawte@gmail.com>
2016-01-28 20:19:42 +1300

    fixed -it time to use correct GPE eqn

--- Baker_Hausdorff_Oh3_iTime.m
+++ Baker_Hausdorff_Oh3_iTime.m
@@ -1,14 +1,14 @@
-function PSIn_iTime = Baker_Hausdorff_Oh3_iTime(PSI,k,g,V,DeltaT)
+function PSIn_iTime = Baker_Hausdorff_Oh3_iTime(PSI,ksquareon2,g,V,DeltaT)
 % Third order symmetry Baker Hausdorff Method with -iDeltaT
 
-BhatPSI = exp(-(g*2*abs(PSI).^2 + V -2) * DeltaT/2) .* PSI ;
+BhatPSI = exp(-(g*abs(PSI).^2 + V -1) * DeltaT/2) .* PSI ;
 %Note the -1 here (= -mu) to remove the trivial phase dependence.
 
-phi = exp(  -k.^2 * DeltaT) .* fft2(BhatPSI);
+phi = exp(-ksquareon2 * DeltaT) .* fft2(BhatPSI);
 
 ABhatPSI = ifft2(phi);
 
-PSIn_iTime = exp(-(g*2*abs(ABhatPSI).^2 + V -2) * DeltaT/2) .* ABhatPSI ;
+PSIn_iTime = exp(-(g*abs(ABhatPSI).^2 + V -1) * DeltaT/2) .* ABhatPSI ;
 %Same here as above 
 
 end
